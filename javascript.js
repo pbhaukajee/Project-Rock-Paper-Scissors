6a13c4a752bc157636da0ff5ac341e911633bbf8
Priyanka <pbhaukajee@gmail.com>
2023-12-11 15:28:51 -0600

    add display result style

--- javascript.js
+++ javascript.js
@@ -7,35 +7,49 @@ const result = document.querySelector(".result");
 const score = document.querySelector(".score");
 const playerScore = document.querySelector(".playerScore");
 const compScore = document.querySelector(".compScore");
+const choice = document.querySelector(".choice");
+const playerOption = document.querySelector(".playerOption");
+const computerOption = document.querySelector(".computerOption");
+
+let yourScore = 0;
+let computerScore = 0;
+let gamePlayed = 0;
+let playerInput;
+
+const click = image.forEach((img) =>
+  img.addEventListener("click", () => {
+    playerInput = `Player: ${img.alt}`;
+    playerOption.textContent = playerInput;
+    let computerInput = getComputerChoice();
+    computerOption.textContent = `Computer: ${computerInput}`;
+  })
+);
 
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3) + 1;
   if (choice == 1) {
-    return "rock";
+    return "Rock";
   } else if (choice == 2) {
-    return "paper";
+    return "Paper";
   } else {
-    return "scissors";
+    return "Scissors";
   }
 }
-let yourScore = 0;
-let computerScore = 0;
-let gamePlayed = 0;
 
 //make a function that shows the final result
 function playRound(playerSelection, computerSelection) {
   let winner;
   if (
-    (playerSelection === "rock" && computerSelection === "paper") ||
-    (playerSelection === "paper" && computerSelection === "scissors") ||
-    (playerSelection === "scissors" && computerSelection === "rock")
+    (playerSelection === "Rock" && computerSelection === "Paper") ||
+    (playerSelection === "Paper" && computerSelection === "Scissors") ||
+    (playerSelection === "Scissors" && computerSelection === "Rock")
   ) {
     winner = "computer";
     computerScore += 1;
   } else if (
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper") ||
-    (playerSelection === "rock" && computerSelection === "scissors")
+    (playerSelection === "Paper" && computerSelection === "Rock") ||
+    (playerSelection === "Scissors" && computerSelection === "Paper") ||
+    (playerSelection === "Rock" && computerSelection === "Scissors")
   ) {
     winner = "player";
     yourScore += 1;
@@ -47,7 +61,6 @@ function playRound(playerSelection, computerSelection) {
 
 function game() {
   for (i = 0; i < 5; i++) {
-    let playerInput = "rock";
     let computerInput = getComputerChoice();
     playRound(playerInput, computerInput);
     gamePlayed += 1;
@@ -67,11 +80,3 @@ function finalwinner() {
     }
   }
 }
-
-game();
-
-image.forEach((img) =>
-  img.addEventListener("click", () => {
-    console.log(img.alt);
-  })
-);
