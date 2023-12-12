e23e8893c1977a42a3062d72cb40d48d279356f3
Priyanka <pbhaukajee@gmail.com>
2023-12-12 09:05:33 -0600

    update score

--- javascript.js
+++ javascript.js
@@ -1,8 +1,4 @@
-const container = document.querySelector(".container");
 const image = document.querySelectorAll("img");
-const rock = document.querySelector(".rock");
-const paper = document.querySelector(".paper");
-const scissors = document.querySelector(".scissors");
 const result = document.querySelector(".result");
 const score = document.querySelector(".score");
 const playerScore = document.querySelector(".playerScore");
@@ -14,16 +10,6 @@ const computerOption = document.querySelector(".computerOption");
 let yourScore = 0;
 let computerScore = 0;
 let gamePlayed = 0;
-let playerInput;
-
-const click = image.forEach((img) =>
-  img.addEventListener("click", () => {
-    playerInput = `Player: ${img.alt}`;
-    playerOption.textContent = playerInput;
-    let computerInput = getComputerChoice();
-    computerOption.textContent = `Computer: ${computerInput}`;
-  })
-);
 
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3) + 1;
@@ -56,27 +42,39 @@ function playRound(playerSelection, computerSelection) {
   } else {
     winner = "tie";
   }
-  return;
-}
-
-function game() {
-  for (i = 0; i < 5; i++) {
-    let computerInput = getComputerChoice();
-    playRound(playerInput, computerInput);
-    gamePlayed += 1;
-  }
-  finalwinner();
-  return;
+  return winner;
 }
 
 function finalwinner() {
-  if (gamePlayed === 5) {
-    if (yourScore > computerScore) {
-      console.log("Yon Won!");
-    } else if (yourScore < computerScore) {
-      console.log("You lost!");
-    } else {
-      console.log("It's a tie!");
-    }
+  if (yourScore > computerScore) {
+    console.log("Yon Won!");
+  } else if (yourScore < computerScore) {
+    console.log("You lost!");
+  } else {
+    console.log("It's a tie!");
   }
+}
+
+image.forEach((img) =>
+  img.addEventListener("click", () => {
+    playerInput = img.alt;
+    playerOption.textContent = `Player: ${img.alt}`;
+    computerInput = getComputerChoice();
+    computerOption.textContent = `Computer: ${computerInput}`;
+    const winner = playRound(playerInput, computerInput);
+    playerScore.textContent = `Player Score: ${yourScore}`;
+    compScore.textContent = `Computer Score: ${computerScore}`;
+    gamePlayed += 1;
+
+    if (gamePlayed === 5) {
+      finalwinner();
+      restart();
+    }
+  })
+);
+
+function restart() {
+  yourScore = 0;
+  computerScore = 0;
+  gamePlayed = 0;
 }
